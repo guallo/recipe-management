4dc55e8dcc176345e8eda8c548baf1a17a0a65a8
guallo <eacuesta1986@gmail.com>
2019-05-15 00:32:23 -0400

    added __str__ methods to models

--- recipe_management/api/models.py
+++ recipe_management/api/models.py
@@ -8,6 +8,9 @@ from recipe_management.api import validators as custom_validators
 class Step(models.Model):
     step_text = models.CharField(max_length=80)
     recipe = models.ForeignKey('Recipe', on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return f'Step<{self.step_text}>{f" of {self.recipe}" if self.recipe is not None else ""}'
 
 
 class Ingredient(models.Model):
@@ -21,8 +24,14 @@ class Ingredient(models.Model):
             # To avoid specify the same ingredient twice for the same recipe.
             constraints.UniqueConstraint(fields=['text', 'recipe'], name='unique_text_recipe'),
         ]
+    
+    def __str__(self):
+        return f'Ingredient<{self.text}>{f" for {self.recipe}" if self.recipe is not None else ""}'
 
 
 class Recipe(models.Model):
     name = models.CharField(max_length=30)
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return f'Recipe<{self.name}>{f" assigned to {self.user}" if self.user is not None else ""}'
